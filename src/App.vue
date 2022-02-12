24428187acc1d6a62714bc58173d3275d5fc1c4d
Towhid Hasan <towhid.hasan004@gmail.com>
2022-02-12 15:56:08 +0600

    Uploaded

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>{{ dateTime.hours }}:{{ dateTime.minutes }}:{{ dateTime.seconds }}</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+const date = new Date();
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      dateTime: {
+        hours: date.getHours(),
+        minutes: date.getMinutes(),
+        seconds: date.getSeconds(),
+      },
+      timer: undefined,
+    };
+  },
+  methods: {
+    setDateTime() {
+      const date = new Date();
+      this.dateTime = {
+        hours: date.getHours(),
+        minutes: date.getMinutes(),
+        seconds: date.getSeconds(),
+      };
+    },
+  },
+  beforeMount() {
+    this.timer = setInterval(this.setDateTime, 1000);
+  },
+  beforeUnmount() {
+    clearInterval(this.timer);
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
